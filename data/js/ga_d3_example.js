db7443b9a6007b598bd1533aa42404a4f6c4bbbb
Christopher Nabors <PillarDD@Gmail.com>
2012-08-20 20:49:50 -0400

    Grouped the SVG

--- data/js/ga_d3_example.js
+++ data/js/ga_d3_example.js
@@ -4,72 +4,77 @@
 // Constants
 var CONTAINER_WIDTH = 300; var CONTAINER_HEIGHT = 300;
 
-var formatted_visitor_response = {
-    "total_visitors": 0,
-    "new_visitors": 0,
-    "returning_visitors": 0
-};
-var circle_visitor_response;
-
-var visitor_response = {
-    "kind": "analytics#gaData",
-    "id": "https://www.googleapis.com/analytics/v3/data/ga?ids=ga:63132366&dimensions=ga:visitorType&metrics=ga:visitors&start-date=2012-08-01&end-date=2012-08-19&start-index=1&max-results=1000",
-    "query": {
-         "start-date": "2012-08-01",
-          "end-date": "2012-08-19",
-           "ids": "ga:63132366",
-            "dimensions": "ga:visitorType",
-             "metrics": [
-                   "ga:visitors"
-                    ],
-              "start-index": 1,
-               "max-results": 1000
-    },
-    "itemsPerPage": 1000,
-    "totalResults": 2,
-    "selfLink": "https://www.googleapis.com/analytics/v3/data/ga?ids=ga:63132366&dimensions=ga:visitorType&metrics=ga:visitors&start-date=2012-08-01&end-date=2012-08-19&start-index=1&max-results=1000",
-    "profileInfo": {
-         "profileId": "63132366",
-          "accountId": "34182323",
-           "webPropertyId": "UA-34182323-1",
-            "internalWebPropertyId": "61644851",
-             "profileName": "CNabors",
-              "tableId": "ga:63132366"
-    },
-    "containsSampledData": false,
-    "columnHeaders": [
-         {
-               "name": "ga:visitorType",
-                 "columnType": "DIMENSION",
-                   "dataType": "STRING"
-                        },
-     {
-           "name": "ga:visitors",
-             "columnType": "METRIC",
-               "dataType": "INTEGER"
-                    }
-    ],
-        "totalsForAllResults": {
-             "ga:visitors": "10"
-        },
-        "rows": [
-             [
-               "New Visitor",
-          "3"
-               ],
-           [
-                 "Returning Visitor",
-             "7"
-                  ]
-                  ]
-};
 // Base JS Logic
 function initialize() {
-    setup_response_array();
-    render_visualization();
+    render_visitor_graphic();
 }
-// Format the initial response into a defined array
-function setup_response_array() {
+function render_visitor_graphic() {
+    /**************************************************************************\
+    | Variables                                                                |
+    \**************************************************************************/
+    var formatted_visitor_response = {
+        "total_visitors": 0,
+        "new_visitors": 0,
+        "returning_visitors": 0
+    };
+    var circle_visitor_response;
+
+    var visitor_response = {
+        "kind": "analytics#gaData",
+        "id": "https://www.googleapis.com/analytics/v3/data/ga?ids=ga:63132366&dimensions=ga:visitorType&metrics=ga:visitors&start-date=2012-08-01&end-date=2012-08-19&start-index=1&max-results=1000",
+        "query": {
+             "start-date": "2012-08-01",
+              "end-date": "2012-08-19",
+               "ids": "ga:63132366",
+                "dimensions": "ga:visitorType",
+                 "metrics": [
+                       "ga:visitors"
+                        ],
+                  "start-index": 1,
+                   "max-results": 1000
+        },
+        "itemsPerPage": 1000,
+        "totalResults": 2,
+        "selfLink": "https://www.googleapis.com/analytics/v3/data/ga?ids=ga:63132366&dimensions=ga:visitorType&metrics=ga:visitors&start-date=2012-08-01&end-date=2012-08-19&start-index=1&max-results=1000",
+        "profileInfo": {
+             "profileId": "63132366",
+              "accountId": "34182323",
+               "webPropertyId": "UA-34182323-1",
+                "internalWebPropertyId": "61644851",
+                 "profileName": "CNabors",
+                  "tableId": "ga:63132366"
+        },
+        "containsSampledData": false,
+        "columnHeaders": [
+             {
+                   "name": "ga:visitorType",
+                     "columnType": "DIMENSION",
+                       "dataType": "STRING"
+                            },
+         {
+               "name": "ga:visitors",
+                 "columnType": "METRIC",
+                   "dataType": "INTEGER"
+                        }
+        ],
+            "totalsForAllResults": {
+                 "ga:visitors": "10"
+            },
+            "rows": [
+                 [
+                   "New Visitor",
+              "3"
+                   ],
+               [
+                     "Returning Visitor",
+                 "7"
+                      ]
+                      ]
+    };
+
+    /**************************************************************************\
+    | Format Response                                                          |
+    \**************************************************************************/
     // Visitor Response
     formatted_visitor_response['new_visitors'] = parseInt(visitor_response['rows'][0][1]);
     formatted_visitor_response['returning_visitors'] = parseInt(visitor_response['rows'][1][1]);
@@ -81,17 +86,25 @@ function setup_response_array() {
             {value: formatted_visitor_response['returning_visitors']}
         ]
     };
-}
-function render_visualization() {
+
+    /**************************************************************************\
+    | D3 Render                                                                |
+    \**************************************************************************/
     // Circle Pack Visitor Response
     var svg = d3.select("body")
         .append("svg")
         .attr("width", CONTAINER_WIDTH)
         .attr("height", CONTAINER_HEIGHT)
-        .attr("style", "padding: 40px;");
+        .style("padding", "40px");
+    
+    var group = d3.select("svg")
+        .append("svg:g")
+        .attr("width", CONTAINER_WIDTH)
+        .attr("height", CONTAINER_HEIGHT)
+        .style("padding", "40px");
 
     // Total Text
-    svg.append("svg:text")
+    group.append("svg:text")
         .attr("text-anchor", "middle")
         .attr("dy", ".5em")
         .attr("x", ((CONTAINER_WIDTH / 2) - 20))
@@ -99,7 +112,7 @@ function render_visualization() {
         .text(function(d) { return ("Total Visitors: " + formatted_visitor_response['total_visitors']); });
     
     // New Text
-    svg.append("svg:text")
+    group.append("svg:text")
         .attr("text-anchor", "left")
         .attr("dy", ".5em")
         .attr("x", -40)
@@ -107,7 +120,7 @@ function render_visualization() {
         .text("New");
     
     // Returning Text
-    svg.append("svg:text")
+    group.append("svg:text")
         .attr("text-anchor", "right")
         .attr("dy", ".45em")
         .attr("x", CONTAINER_WIDTH - 35)
@@ -118,7 +131,7 @@ function render_visualization() {
         .pack()
         .size([CONTAINER_WIDTH - 40, CONTAINER_HEIGHT - 40]);
 
-    var circ = svg.selectAll("circle")
+    var circ = group.selectAll("circle")
         .data(pack.nodes(circle_visitor_response))
         .enter();
     
